a7b18f5edb667354a28dc1dd952d2539d892c2c6
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-05-11 17:34:00 +0300

    Done

--- app.js
+++ app.js
@@ -9,4 +9,81 @@ app.get("/todo",(req,res)=>{
     res.status(200);
     res.json(todos);
 });
+app.post("/create/todo",(req,res)=>{
+    const newTodo = { todo:req.body.todo, isCompleted:req.body.isCompleted};
+    todos.push(newTodo);
+    res.status(201);
+    res.json(newTodo);
+    
+    });
 
+    app.put("/update/todo/:name",(req,res)=>{
+
+        const todo = req.params.name
+
+        const found = todos.find((element)=>{
+            return element.todo === todo ;
+        });
+     
+    
+      if (found){
+        res.status(200);
+        const newTodo = { todo:req.body.todo, isCompleted:req.body.isCompleted};
+
+          res.json(newTodo);
+      }else {
+          res.status(404);
+          res.json("todo not found")
+      }
+    });
+
+    app.delete("/delete/todo/:name",(req,res)=>{
+       let index;
+        const todo = req.params.name
+
+        const found = todos.find((element,i)=>{
+            index = i;
+            return element.todo === todo ;
+    });
+    if(found){
+
+        todos.splice(index,1);
+        res.status(200);
+        res.json(todos);
+    } else {
+        res.status(404);
+        res.json("todo not found");
+    }
+} );
+
+app.put('/complete/todo/:name', (req,res)=>{
+    let index;
+    const todo = req.params.name;
+    const found = todos.find((element,i)=>{
+        index = i;
+        return element.todo === todo;
+    });
+    if(found){
+        todos[index].isCompleted = true;
+        res.status(200);
+        res.json(todos);
+
+    } else {
+        res.status(404);
+        res.json("todo not found ")
+    }
+});
+
+
+app.get('/completed/todos', (req,res)=>{
+    const found = todos.filter((element,i)=>{
+        return element.isCompleted === true;
+    })
+    res.json(found);
+
+});
+
+
+ app.listen(port,()=>{
+    console.log(`the server run on http://localhost:${port}`)
+      });
